e477645e153f64aa62d01cade2577cfbe3856dd3
Josoa Vonjiniaina <76913187+josoavj@users.noreply.github.com>
2025-10-18 19:49:49 +0300

    update : Ajout des infos génerales sur le script SQL comme l'auteur et aussi la date de modif

---
+++ scriptSQL/Prospectius.sql
@@ -0,0 +1,13 @@
+/*
+    Auteur: Josoa (josoavj sur GitHub)
+    Ce script est la source de la base de données du projet Prospectius
+    Veuillez vous réferer à la documentation ou envoyer un mail à l'auteur si vous avez besoin d'aide
+*/
+
+-- Suppression complète de la base de données
+DROP DATABASE IF EXISTS Prospectius;
+
+
+/*
+    Modifié le 18 Octobre 2025
+*/
