cc0be3b0f5779746428ed59c57563c5450049ea7
Josoa Vonjiniaina <76913187+josoavj@users.noreply.github.com>
2025-10-23 13:40:35 +0300

    update: Mise à jour du script de suppression de la base de données

--- scriptSQL/SuppressionDB.sql
+++ scriptSQL/SuppressionDB.sql
@@ -1,5 +1,13 @@
-# Suppression de la base de données entière
-DROP DATABASE Prospectius;
+/*
+    Scripts de suppression des tables et de la base de donneés
+*/
+
+
 
 # Suppression des Tables
-DROP TABLE Account;
+DROP TABLE IF EXISTS Account;
+DROP TABLE IF EXISTS Prospect;
+DROP TABLE IF EXISTS Interaction;
+
+# Suppression de la base de données entière
+DROP DATABASE IF EXISTS Prospectius;
